9befddbf54e81768a67c9befc8b6258fa9421124
Melvin Laplanche <melvin.wont.reply@gmail.com>
2023-09-04 16:20:12 -0700

    Add better linter rules

--- ui/packages/eslint-config-coeur/index.js
+++ ui/packages/eslint-config-coeur/index.js
@@ -1,11 +1,53 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
+  rules: {
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [['^\\u0000'], ['^@?\\w'], ['^'], ['^\\.', '^(@/)']],
+      },
+    ],
+    'simple-import-sort/exports': ['warn'],
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'prettier/prettier': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/no-default-export': 'error',
+    // "@typescript-eslint/no-floating-promises": "error",
+    // "@typescript-eslint/await-thenable": "error",
+    // "@typescript-eslint/no-misused-promises": "error"
+  },
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
 };
